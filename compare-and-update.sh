f515b4b2b10630ac07fab3f2f20729a9d9bf2ff1
Chris Partridge <chris@partridge.tech>
2019-12-25 17:16:12 -0500

    big idiot energy

--- compare-and-update.sh
+++ compare-and-update.sh
@@ -6,11 +6,11 @@ files=( "icann.json" "private.json" )
 for file in "${files[@]}"
 do
   if cmp running/$file psd/$file; then
+    echo "$file is the same in both places"
+  else
     echo "$file differs, the psd needs to be updated"
     cp -fv running/* psd/
     echo "local psd directory can be used to push changes"
     exit 1
-  else
-    echo "$file is the same in both places"
   fi
 done
